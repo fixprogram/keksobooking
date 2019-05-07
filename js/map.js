fa27aea6a5bfeb55bf620842a4a2b1e4cf8a0d41
Denis <fixprogram@mail.ru>
2019-05-07 08:52:36 +0300

    From 07.05.19. Taks number three was compeled. All problems from previous commit were solved

--- js/map.js
+++ js/map.js
@@ -6,18 +6,74 @@
   let mapPins = document.querySelector('.map__pins');
   let cards = [];
   let btnPopupClose = "";
-  let mapMainPin = document.querySelector('.map__pin--main');
+  let buildingTypes = {
+    flat: 'Квартира',
+    bungalo: 'Бунгало',
+    house: 'Дом',
+    palace: 'Дворец'
+  };
 
-  let createEl = function(tagName, className, content, src, alt) {
+  let createEl = function(tagName, className, content, addition) {
     let el = document.createElement(tagName);
     el.classList.add(className);
-    if(tagName === 'img') {
-      el.src = src;
-      el.alt = alt;
+
+    if(tagName === 'p') {
+      el.classList.add('popup__text');
     }
-    el.textContent = content;
+
+    if(className === 'popup__text--price') {
+      el.textContent = content + ' ₽/ночь';
+    } else if(className === 'popup__text--capacity') {
+      el.textContent = content + ' комнаты для ' + addition + ' гостей';
+    } else if(className === 'popup__text--time') {
+      el.textContent = 'Заезд после ' + content + ', выезд до ' + addition;
+    } else {
+      el.textContent = content;
+    }
     return el;
-    };
+  };
+
+  let createType = function(tagName, className, type) {
+    let typeEl = document.createElement(tagName);
+    typeEl.classList.add(className);
+    typeEl.textContent = buildingTypes[type];
+    return typeEl;
+  };
+
+  let createImg = function(className, src, alt) {
+    let imgItem = document.createElement('img');
+    imgItem.classList.add(className);
+    imgItem.src = src;
+    imgItem.alt = alt;
+    return imgItem;
+  };
+
+  let createGallery = function(tagName, className, classChildName, photos, alt) {
+    let gallery = document.createElement(tagName);
+    gallery.classList.add(className);
+    for(let i = 0; i < photos.length; i++) {
+      let photo = document.createElement('img');
+      photo.classList.add(classChildName);
+      photo.src = photos[i];
+      photo.width = 45;
+      photo.height = 40;
+      photo.alt = alt;
+      gallery.appendChild(photo);
+    }
+    return gallery;
+  };
+
+  let createList = function(className, classChildName, array) {
+    let list = document.createElement('ul');
+    list.classList.add(className);
+    for(let i = 0; i < array.length; i++) {
+      let listEl = document.createElement('li');
+      listEl.classList.add(classChildName);
+      listEl.classList.add(classChildName + '--' + array[i]);
+      list.appendChild(listEl);
+    }
+    return list;
+  };
 
   let createPin = function(data) {
     let pinBtn = document.createElement('button');
@@ -36,16 +92,17 @@
     let card = document.createElement('article');
     card.classList.add('map__card');
     card.classList.add('popup');
-    card.appendChild(createEl('img', 'popup__avatar', '', data.author.avatar, 'Аватар пользователя'));
+    card.appendChild(createImg('popup__avatar', data.author.avatar, 'Аватар пользователя'));
     card.appendChild(createEl('button', 'popup__close', 'Закрыть'));
+    card.appendChild(createEl('h3', 'popup__title', data.offer.title));
     card.appendChild(createEl('p', 'popup__text--address', data.offer.address));
     card.appendChild(createEl('p', 'popup__text--price', data.offer.price));
-    card.appendChild(createEl('h4', 'popup__type', data.offer.title));
-    card.appendChild(createEl('p', 'popup__text--capacity', data.offer));
-    card.appendChild(createEl('p', 'popup__text--time', data.offer.checkout));
-    // create list of features
+    card.appendChild(createType('h4', 'popup__type', data.offer.type));
+    card.appendChild(createEl('p', 'popup__text--capacity', data.offer.rooms, data.offer.guests));
+    card.appendChild(createEl('p', 'popup__text--time', data.offer.checkin, data.offer.checkout));
+    card.appendChild(createList('popup__features', 'popup__feature', data.offer.features));
     card.appendChild(createEl('p', 'popup__description', data.offer.description));
-    // create photos gallery
+    card.appendChild(createGallery('div', 'popup__photos', 'popup__photo', data.offer.photos, 'Фотография жилья'));
     cards.push(card);
     map.appendChild(card);
     return card;
@@ -60,10 +117,10 @@
   let addClickListener = function(mapPin, data) {
       mapPin.addEventListener('click', function(evt){
         let popup = document.querySelector('.map__card');
+        let card = createCard(data);
         if(popup) {
           map.removeChild(popup);
         }
-        let card = createCard(data);
         console.log(mapPin);
         btnPopupClose = document.querySelector('.popup__close');
         btnPopupClose.addEventListener('click', function(evt){
@@ -79,15 +136,13 @@
 
   let onSuccess = function(data) {
     let adData = data;
-    // for(let i = 0; i < adData.length; i++) {
-    //   createCard(adData[i]);
-    // }
     window.renderPinsMarkup(adData);
+    console.log(adData);
     let mapPin = document.querySelectorAll('.map__pin');
-    for(let i = 1; i < adData.length; i++) {
-      let data = adData[i];
-      let pin = mapPin[i];
-      addClickListener(pin, data);
+    let pinsArray = Array.prototype.slice.call(mapPin);
+    pinsArray.shift();
+    for(let i = 0; i < pinsArray.length; i++) {
+      addClickListener(pinsArray[i], adData[i]);
     }
   };
 
@@ -109,11 +164,7 @@
 
   pinMain.addEventListener('click', function(){
     renderMap();
+    pinMain.disabled = true;
   });
-
-  let popupClose = function() {
-    let card = document.querySelector('.map__card');
-    map.removeChild(card);
-  };
 
 })();
